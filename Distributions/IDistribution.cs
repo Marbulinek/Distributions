9d733ac24aa226c73cd0600b1f52e5b895a7ba9d
Lukáš Čaniga <lukas.caniga@gmail.com>
2024-11-14 14:06:01 +0100

    Added missing documentation

--- Distributions/IDistribution.cs
+++ Distributions/IDistribution.cs
@@ -1,7 +1,13 @@
 ﻿namespace Distributions
 {
+    /// <summary>
+    /// Represents a distribution interface that provides a method to get a distribution value.
+    /// </summary>
     public interface IDistribution
     {
+        /// <summary>
+        /// Represents a distribution interface that provides a method to get a distribution value.
+        /// </summary>
         double GetDistribution();
     }
 }
